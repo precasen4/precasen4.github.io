0d3237bc334ebf3c87cd40e7217776a0ea39a688
precasen4 <precasen4@iespompeufabra.cat>
2026-01-14 19:24:15 +0100

    Cambios examen

--- 2013/questions.js
+++ 2013/questions.js
@@ -2,190 +2,211 @@ const questions = [
     {
         type: "exercicis",
         category: "energia",
-        text: `S’han instal·lat \\(n = 50\\) aerogeneradors de 3 pales en un parc eòlic. 
-               El diàmetre de l’àrea d’escombratge de les pales és \\(d = 77 \\, \\text{m}\\) i el rendiment dels aerogeneradors \\(\\eta_{\\text{aerog}} = 0{,}68\\). 
-               S’estima que la velocitat mitjana del vent al parc és \\(v = 25 \\, \\text{km/h}\\); el parc està en funcionament 300 dies a l’any i obté energia 18 hores diàries. 
-               La potència mitjana del vent \\(P_{\\text{vent}}\\) es pot estimar, per a un aerogenerador, com l’energia cinètica del vent per unitat de temps mitjançant l’expressió:
-               \\[
-               P_{\\text{vent}} = \\frac{1}{2} \\rho A v^3
-               \\]
-               en què \\(A\\) és l’àrea que escombren les pales de l’aerogenerador, \\(\\rho\\) és la densitat de l’aire i \\(v\\) és la velocitat del vent. 
-               D’aquesta potència, el coeficient d’aprofitament del vent és \\(c_a = 0{,}42\\).
-               
-               Sabent que \\(1{,}225 \\, \\text{g}\\) d’aire ocupen un volum d’\\(1 \\, \\text{m}^3\\), determineu:
-               <br><strong>a)</strong> La potència mitjana del vent \\(P_{\\text{vent}}\\) per a un aerogenerador. [0,5 punts]
-               <br><strong>b)</strong> La potència elèctrica útil \\(P_{\\text{útil}}\\) que generarà cada aerogenerador. [0,5 punts]
-               <br><strong>c)</strong> L’energia elèctrica total \\(E_{\\text{total}}\\) que es generarà al parc durant un any. [0,5 punts]
-               <br><strong>d)</strong> Els ingressos que tindrà el parc eòlic si el preu de venda de l’energia eòlica és \\(p_{\\text{venda}} = 7{,}624\\) cèntims d’euro per \\(\\text{kWh}\\). [0,5 punts]
-               <br><strong>e)</strong> En quin percentatge disminuirien els ingressos anteriors si la velocitat mitjana del vent es reduís un 10 %.
-               <br><br>
-               <img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`,  // Imatge opcional al final de la pregunta
-        correctAnswer: "",  // No és necessari aquí perquè es tracta d'un exercici obert
+        text: `<p>
+El sistema d’alarma d’un habitatge consta de tres detectors de moviment instal·lats estratègicament per la casa.
+L’alarma s’activa quan dos dels tres detectors, com a mínim, han detectat algun moviment.
+Responeu a les qüestions que hi ha a continuació utilitzant les variables d’estat següents:
+</p>
+
+<p>
+detector 1: d<sub>1</sub> = { 1: moviment detectat 
+0: no detectat }
+</p>
+
+<p>
+detector 2: d<sub>2</sub> = { 1: moviment detectat 
+0: no detectat }
+</p>
+
+<p>
+detector 3: d<sub>3</sub> = { 1: moviment detectat
+0: no detectat }
+</p>
+
+<p>
+alarma: a = { 1: activada 0: no activada }
+</p>
+
+<p>
+<strong> a) </strong> Escriu la taula de veritat del sistema. [1 punt]
+</p>
+
+<p>
+<strong> c) </strong> Determineu la funció lògica entre aquestes variables i, si escau, simplifiqueu-la.
+[1 punt]
+</p>
+
+<p>
+<strong> c) </strong> Dibuixeu l’esquema de portes lògiques equivalent. [0,5 punts]
+</p>
+
+               `, 
+        correctAnswer: "",  
         steps: `
-            <strong>a) Càlcul de la potència mitjana del vent \\(P_{\\text{vent}}\\) per a un aerogenerador:</strong>
-            <br>
-            - Àrea d’escombratge de les pales:
-            \\[
-            A = \\frac{\\pi d^2}{4} = \\frac{\\pi \\times (77 \\, \\text{m})^2}{4} = 4657{,}7 \\, \\text{m}^2
-            \\]
-            - Densitat de l’aire:
-            \\[
-            \\rho = \\frac{1{,}225 \\, \\text{g}}{\\text{m}^3} = 1{,}225 \\, \\frac{\\text{kg}}{\\text{m}^3}
-            \\]
-            - Velocitat del vent (en \\(\\text{m/s}\\)):
-            \\[
-            v = \\frac{25 \\, \\text{km/h}}{3{,}6} = 6{,}94 \\, \\text{m/s}
-            \\]
-            - Potència del vent:
-            \\[
-            P_{\\text{vent}} = \\frac{1}{2} \\times 1{,}225 \\, \\frac{\\text{kg}}{\\text{m}^3} \\times 4657{,}7 \\, \\text{m}^2 \\times (6{,}94 \\, \\text{m/s})^3 = 828{,}08 \\, \\text{kW}
-            \\]
-            <br>
-            <strong>b) Potència elèctrica útil \\(P_{\\text{útil}}\\) generada per cada aerogenerador:</strong>
-            <br>
-            - Aplicant el rendiment de l’aerogenerador:
-            \\[
-            P_{\\text{útil}} = P_{\\text{vent}} \\times \\eta_{\\text{aerog}} \\times c_a = 828{,}08 \\, \\text{kW} \\times 0{,}68 \\times 0{,}42 = 236{,}62 \\, \\text{kW}
-            \\]
-            <br>
-            <strong>c) Energia elèctrica total \\(E_{\\text{total}}\\) generada al parc durant un any:</strong>
-            <br>
-            - Nombre total d'hores operatives a l'any:
-            \\[
-            h = 300 \\, \\text{dies/any} \\times 18 \\, \\text{h/dia} = 5400 \\, \\text{h/any}
-            \\]
-            - Energia generada per un aerogenerador:
-            \\[
-            E_{\\text{útil}} = P_{\\text{útil}} \\times h = 236{,}62 \\, \\text{kW} \\times 5400 \\, \\text{h} = 1277{,}75 \\, \\text{MWh/any}
-            \\]
-            - Energia total generada per 50 aerogeneradors:
-            \\[
-            E_{\\text{total}} = 1277{,}75 \\, \\text{MWh/any} \\times 50 = 63{,}89 \\, \\text{GWh/any}
-            \\]
-            <br>
-            <strong>d) Ingressos del parc eòlic:</strong>
-            <br>
-            - Ingressos totals:
-            \\[
-            Ingressos = E_{\\text{total}} \\times p_{\\text{venda}} = 63{,}89 \\, \\text{GWh/any} \\times 7{,}624 \\, \\text{cèntims/kWh} = 4865{,}9 \\, \\text{k€}
-            \\]
-            <br>
-            <strong>e) Impacte d'una reducció del 10 % en la velocitat del vent:</strong>
-            <br>
-            - Nova velocitat del vent:
-            \\[
-            v_{nou} = 0{,}9 \\times v = 0{,}9 \\times 6{,}94 \\, \\text{m/s} = 6{,}25 \\, \\text{m/s}
-            \\]
-            - Nova potència del vent:
-            \\[
-            P_{\\text{vent, nou}} = \\frac{1}{2} \\times 1{,}225 \\, \\frac{\\text{kg}}{\\text{m}^3} \\times 4657{,}7 \\, \\text{m}^2 \\times (6{,}25 \\, \\text{m/s})^3 = 654{,}8 \\, \\text{kW}
-            \\]
-            - Nova potència útil:
-            \\[
-            P_{\\text{útil, nou}} = 654{,}8 \\, \\text{kW} \\times 0{,}68 \\times 0{,}42 = 187{,}19 \\, \\text{kW}
-            \\]
-            - Nova energia total:
-            \\[
-            E_{\\text{total, nou}} = 187{,}19 \\, \\text{kW} \\times 5400 \\, \\text{h} \\times 50 = 50{,}7 \\, \\text{GWh/any}
-            \\]
-            - Nous ingressos:
-            \\[
-            Ingressos_{nou} = 50{,}7 \\, \\text{GWh/any} \\times 7{,}624 \\, \\text{cèntims/kWh} = 3865{,}58 \\, \\text{k€}
-            \\]
-            - Percentatge de disminució:
-            \\[
-            \\frac{4865{,}9 - 3865{,}58}{4865{,}9} \\times 100 \\approx 20{,}6\\%
-            \\]
-            <br><br>
-            <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
+            <img src="exercici2.png" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
     },
     {
-        type: "questions", // Tipus de pregunta
-        category: "energia",
-        text: `Un rentaplats ha consumit \\(0{,}9 \\, \\text{kWh}\\) d’energia elèctrica per a escalfar \\(11 \\, \\text{L}\\) d’aigua des d’una temperatura ambient \\(T_{amb} = 10 \\, ^\\circ \\text{C}\\) a una temperatura \\(T = 70 \\, ^\\circ \\text{C}\\). La calor específica de l’aigua és \\(c_e = 4{,}18 \\, \\frac{\\text{J}}{\\text{g} \\cdot ^\\circ \\text{C}}\\). El rendiment del rentaplats és:`,
+        type: "questions", 
+        category: "geometria",
+        text: `Un cargol M20 × 2,5 normalitzat té un diàmetre exterior de 20 mm i un pas de 2,5 mm. Si es cargola en una femella fixa, quantes voltes ha de donar per a avançar 5 mm?`,
         options: [
-            { text: "11,74 %", value: "a" },
-            { text: "27,59 %", value: "b" },
-            { text: "76,63 %", value: "c" },
-            { text: "85,15 %", value: "d" }
-        ],
-        correctAnswer: "d",
-        steps: `
-            \\[
-            m = 11 \\, \\text{L} \\times 1000 \\, \\text{g/L} = 11000 \\, \\text{g}
-            \\]
-            \\[
-            \\Delta T = 70 \\, ^\\circ \\text{C} - 10 \\, ^\\circ \\text{C} = 60 \\, ^\\circ \\text{C}
-            \\]
-            \\[
-            Q = m \\times c_e \\times \\Delta T
-            \\]
-            \\[
-            Q = 11000 \\, \\text{g} \\times 4{,}18 \\, \\frac{\\text{J}}{\\text{g} \\cdot ^\\circ \\text{C}} \\times 60 \\, ^\\circ \\text{C}
-            \\]
-            \\[
-            Q = 11000 \\times 4{,}18 \\times 60 = 2{,}7588 \\times 10^6 \\, \\text{J}
-            \\]
-            \\[
-            W = 0{,}9 \\, \\text{kWh} \\times 3600000 \\, \\text{J/kWh} = 3240000 \\, \\text{J}
-            \\]
-            \\[
-            \\eta = \\frac{Q}{W} \\times 100
-            \\]
-            \\[
-            \\eta = \\frac{2{,}7588 \\times 10^6}{3{,}24 \\times 10^6} \\times 100 = 85{,}15 \\, \\%
-            \\]
-            <br><br>
-            <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
-        images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
-    },
-    {
-        type: "questions", // Tipus de pregunta
-        category: "materials",
-        text: `Una família que anualment generava 525 kg d’envasos, en un any ha aconseguit reduir aquesta quantitat un 60 %. Quina reducció en emissions de GEH ha obtingut respecte a l’any anterior, tenint en compte que el factor d’emissió dels envasos és de 120,09 g CO2eq/kg residu?`,
-        options: [
-            { text: "25,22 kg CO2eq", value: "a" },
-            { text: "37,83 kg CO2eq", value: "b" },
-            { text: "63,05 kg CO2eq", value: "c" },
-            { text: "88,27 kg CO2eq", value: "d" }
+            { text: "4", value: "a" },
+            { text: "2", value: "b" },
+            { text: "8π", value: "c" },
+            { text: "4π", value: "d" }
         ],
         correctAnswer: "b",
+    },
+    {
+        type: "questions",
+        category: "materials",
+        text: `L’aliatge de Devarda conté un 49 % de coure (Cu), un 5 % de zinc (Zn) i alumini (Al).
+Amb 325,5 kg d’alumini, quina quantitat d’aliatge podem obtenir?`,
+        options: [
+            { text: "149,7 kg", value: "a" },
+            { text: "638,2 kg", value: "b" },
+            { text: "707,6 kg", value: "c" },
+            { text: "166,0 kg", value: "d" }
+        ],
+        correctAnswer: "c",
+    },{
+        type: "questions",
+        category: "electrics",
+        text: `El procés d’obtenció d’una peça requereix dues operacions que duren respectivament
+14 s i 12 s. Si es vol obtenir una peça cada 6 s quan el procés funcioni en règim estacionari al
+màxim rendiment, quantes màquines es necessiten per a dur a terme cada operació?`,
+        options: [
+            { text: "1 màquina per a la primera operació i 2 màquines per a la segona operació.", value: "a" },
+            { text: "2 màquines per a la primera operació i 3 màquines per a la segona operació.", value: "b" },
+            { text: "2 màquines per a la primera operació i 2 màquines per a la segona operació.", value: "c" },
+            { text: "3 màquines per a la primera operació i 2 màquines per a la segona operació.", value: "d" }
+        ],
+        correctAnswer: "d",
+    },{
+        type: "questions",
+        category: "motors",
+        text: `Un motor asíncron de corrent altern té una velocitat de sincronisme de 1 500 min–1 quan
+es connecta a una xarxa de tensió U = 230 V i freqüència f = 50 Hz. Si es connecta aquest
+mateix motor a una xarxa de tensió U = 230 V i freqüència f = 60 Hz, quina velocitat de sincronisme tindrà?`,
+        options: [
+            { text: "1 250 min–1", value: "a" },
+            { text: "1 500 min–1", value: "b" },
+            { text: "1 800 min–1", value: "c" },
+            { text: "No es pot determinar sense saber el lliscament relatiu", value: "d" }
+        ],
+        correctAnswer: "c",
+    },{
+        type: "questions",
+        category: "energia",
+        text: `Una joguina s’alimenta amb dues piles AA de capacitat de càrrega c = 2 100 mA h que
+proporcionen una tensió U = 1,5 V. Les piles es connecten en sèrie de manera que proporcionen una tensió de 3 V a la joguina. Quina és l’energia elèctrica que té la joguina per a funcionar?`,
+        options: [
+            { text: "3,15 W h", value: "a" },
+            { text: "6,3 W h", value: "b" },
+            { text: "9,45 W h", value: "c" },
+            { text: "12,6 W h", value: "d" }
+        ],
+        correctAnswer: "b",
+    },
+    {
+        type: "exercicis",
+        category: "energia",
+        text: `<p>
+Un grup electrogen de gas natural està format per un motor i un alternador elèctric trifàsic
+de factor de potència cos φ = 0,8. L’alternador està unit directament a l’eix del motor. El
+full de característiques del grup dóna, entre altres, les dades nominals següents:
+</p>
+
+<p>
+<strong>Potència elèctrica:</strong> P<sub>e</sub> = 32 kW<br>
+<strong>Tensió:</strong> U = 230 V<br>
+<strong>Factor de potència:</strong> cos φ = 0,8<br>
+<strong>Velocitat de gir:</strong> n = 1 500 min<sup>−1</sup><br>
+<strong>Potència del motor:</strong> P<sub>motor</sub> = 36 kW<br>
+<strong>Consum específic del motor:</strong> c<sub>e</sub> = 383 g/(kW h)
+</p>
+
+<p>
+El poder calorífic del gas natural és p<sub>c</sub> = 31 MJ/kg. Determineu:
+</p>
+
+<p>
+<strong>a)</strong> El rendiment de l’alternador, η<sub>alternador</sub>. [0,5 punts]<br>
+<strong>b)</strong> El rendiment del motor, η<sub>motor</sub>. [1 punt]<br>
+<strong> c)</strong> La intensitat que subministra l’alternador (recordeu que per al corrent trifàsic
+P<sub>e</sub> = U I cos φ). [0,5 punts]<br>
+<strong> d)</strong> L’energia dissipada en t = 4 h de funcionament. [0,5 punts]
+</p>`,  
+        correctAnswer: "",  
         steps: `
-            \\[
-            \\text{Quantitat inicial d’envasos} = 525 \\, \\text{kg}
-            \\]
-            \\[
-            \\text{Percentatge de reducció} = 60\\%
-            \\]
-            \\[
-            \\text{Quantitat reduïda} = 525 \\, \\text{kg} \\times 0{,}60 = 315 \\, \\text{kg}
-            \\]
-            \\[
-            \\text{Quantitat de residu restant} = 525 \\, \\text{kg} - 315 \\, \\text{kg} = 210 \\, \\text{kg}
-            \\]
-            \\[
-            \\text{Emissions inicials} = 525 \, \\text{kg} \\times 120{,}09 \, \\text{g CO2eq/kg}
-            \\]
-            \\[
-            \\text{Emissions inicials} = 63047,25 \, \\text{g CO2eq}
-            \\]
-            \\[
-            \\text{Emissions inicials} = 63{,}05 \, \\text{kg CO2eq}
-            \\]
-            \\[
-            \\text{Emissions després de la reducció} = 210 \, \\text{kg} \\times 120{,}09 \, \\text{g CO2eq/kg}
-            \\]
-            \\[
-            \\text{Emissions després de la reducció} = 25218{,}9 \, \\text{g CO2eq}
-            \\]
-            \\[
-            \\text{Emissions després de la reducció} = 25{,}22 \, \\text{kg CO2eq}
-            \\]
-            \\[
-            \\text{Reducció en emissions} = 63{,}05 \, \\text{kg CO2eq} - 25{,}22 \, \\text{kg CO2eq} = 37{,}83 \, \\text{kg CO2eq}
-            \\]
-            <br><br>
-            <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
-        images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
-    }
+            <img src="Exercici3.png" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
+    },
+    {
+        type: "exercicis",
+        category: "energia",
+        text: `<p>
+Una impressora 3D permet fabricar peces de plàstic a base d’anar dipositant capes horitzontals
+de gruix e = 0,5 mm. S’alimenta amb un filament d’àcid polilàctic (PLA) de diàmetre
+d = 3 mm i densitat ρ = 1 250 kg/m<sup>3</sup> que passa per un extrusor, on s’escalfa i es
+prem perquè es dipositi adequadament.
+</p>
+
+<p>
+Amb aquesta tecnologia es fabrica el sòlid massís de la figura. <strong>Determineu:</strong>
+</p>
+
+<p>
+a) La massa, m, del sòlid construït. [1 punt]<br>
+b) La longitud, L, del filament utilitzat. [1 punt]<br>
+c) El nombre de capes que ha dipositat la impressora. [0,5 punts]
+</p>
+
+
+<img src="Exercici4_ajuda.png" alt="Imatge relacionada amb la resposta">`,  
+        correctAnswer: "",  
+        steps: `
+            <img src="Exercici4.png" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
+    }, {
+        type: "exercicis",
+        category: "energia",
+        text: `
+        <p>
+                <img src="Exercici3b.png" alt="Imatge relacionada amb la resposta">
+</p>
+<p>
+La plataforma elevadora de tisora de la figura s’utilitza per a elevar un vehicle de massa
+m = 1 500 kg mitjançant l’acció de dos cilindres hidràulics que actuen en paraŀlel. Els cilindres
+tenen un diàmetre interior d<sub>int</sub> = 100 mm i el diàmetre de la tija és d<sub>tija</sub> = 62 mm.
+</p>
+
+<p>
+La plataforma funciona per a valors 0 mm &lt; h &lt; 1 150 mm i, per a aquest rang de funcionament,
+la relació entre la velocitat d’allargament del cilindre, v<sub>cil</sub>, i la velocitat d’elevació del vehicle,
+v<sub>veh</sub>, és aproximadament:
+</p>
+
+<p>
+<img src="Exercici3b_ajut2.png"> amb h en mm.
+</p>
+
+<p>
+<strong> a)</strong>  Dibuixeu, de manera aproximada i indicant les escales, la relació v<sub>cil</sub>/v<sub>veh</sub> en funció de
+h per a 0 mm &lt; h &lt; 1 150 mm. [1 punt]
+</p>
+
+<p>
+Si les resistències passives es consideren negligibles i el vehicle puja a velocitat constant,
+<strong>determineu, quan h = 800 mm:</strong>
+</p>
+
+<p>
+<strong> b) </strong> La força, F<sub>cil</sub>, que fa el conjunt dels dos cilindres. [1 punt]<br>
+<strong> c) </strong> La pressió relativa, p<sub>int</sub>, a l’interior dels cilindres. [0,5 punts]
+</p>
+
+               `, 
+        correctAnswer: "",  
+        steps: `
+            <img src="exercici3b_solucio.png" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
+    },
 ];
